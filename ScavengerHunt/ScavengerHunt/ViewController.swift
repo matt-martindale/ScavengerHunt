a857554f8849a8c6cd9c59c1325b2e839a334e38
Matt Martindale <martindalematt18@yahoo.com>
2021-03-09 18:36:51 -0700

    Added project files

---
+++ ScavengerHunt/ScavengerHunt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScavengerHunt
+//
+//  Created by Matthew Martindale on 3/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
