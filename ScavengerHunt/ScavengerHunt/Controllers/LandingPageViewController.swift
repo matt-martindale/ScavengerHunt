64351f717bb9685583213f040b161c58ef16eb87
Matt Martindale <martindalematt18@yahoo.com>
2021-03-10 18:43:31 -0700

    Renamed storyboard, added playButton asset

---
+++ ScavengerHunt/ScavengerHunt/Controllers/LandingPageViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LandingPageViewController.swift
+//  ScavengerHunt
+//
+//  Created by Matthew Martindale on 3/10/21.
+//
+
+import UIKit
+
+class LandingPageViewController: UIViewController {
+
+    @IBOutlet weak var playButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    private func setupViews() {
+    }
+
+}
