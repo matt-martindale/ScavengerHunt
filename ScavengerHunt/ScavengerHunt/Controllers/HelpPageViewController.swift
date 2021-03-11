d2cb748206004da0ae87943ec6612c5348516a9d
Matt Martindale <martindalematt18@yahoo.com>
2021-03-10 19:57:30 -0700

    Added HelpPageVC

---
+++ ScavengerHunt/ScavengerHunt/Controllers/HelpPageViewController.swift
@@ -0,0 +1,22 @@
+//
+//  HelpPageViewController.swift
+//  ScavengerHunt
+//
+//  Created by Matthew Martindale on 3/10/21.
+//
+
+import UIKit
+
+class HelpPageViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var button: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
