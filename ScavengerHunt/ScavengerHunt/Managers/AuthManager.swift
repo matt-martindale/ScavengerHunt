8335cc349d3c38c7a6cf2107c778c0c185fb6b02
Matt Martindale <martindalematt18@yahoo.com>
2021-03-11 21:38:17 -0700

    Created AuthManager struct

---
+++ ScavengerHunt/ScavengerHunt/Managers/AuthManager.swift
@@ -0,0 +1,15 @@
+//
+//  AuthManager.swift
+//  ScavengerHunt
+//
+//  Created by Matthew Martindale on 3/11/21.
+//
+
+import Foundation
+import FirebaseAuth
+
+struct AuthManager {
+    static let shared = AuthManager()
+    
+    private init () {}
+}
