b1e52926a3e5bfba63a1d7ff4f79c9f187cb298c
Matt Martindale <martindalematt18@yahoo.com>
2021-03-11 19:37:36 -0700

    Created CreatorLoginVC with basic login form

---
+++ ScavengerHunt/ScavengerHunt/Controllers/CreatorControllers/CreatorLoginViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CreatorLoginViewController.swift
+//  ScavengerHunt
+//
+//  Created by Matthew Martindale on 3/11/21.
+//
+
+import UIKit
+
+class CreatorLoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
