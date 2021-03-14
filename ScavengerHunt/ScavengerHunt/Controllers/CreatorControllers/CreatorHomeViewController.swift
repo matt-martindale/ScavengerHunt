246a31bd0ab8d5439a92cef0111dc757698028e0
Matt Martindale <martindalematt18@yahoo.com>
2021-03-13 19:16:06 -0700

    Added CreatorHomeVC

---
+++ ScavengerHunt/ScavengerHunt/Controllers/CreatorControllers/CreatorHomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CreatorHomeViewController.swift
+//  ScavengerHunt
+//
+//  Created by Matthew Martindale on 3/13/21.
+//
+
+import UIKit
+
+class CreatorHomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
