ae6a1757b6aba6a97b91998733f8c492d2068500
Matt Martindale <martindalematt18@yahoo.com>
2021-03-24 18:36:29 -0600

    Added PlayerFormVC

---
+++ ScavengerHunt/ScavengerHunt/Controllers/PlayerFormViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PlayerFormViewController.swift
+//  ScavengerHunt
+//
+//  Created by Matthew Martindale on 3/24/21.
+//
+
+import UIKit
+
+class PlayerFormViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
