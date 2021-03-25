53d74224011692499021aee27ba69e909b3793e9
Matt Martindale <martindalematt18@yahoo.com>
2021-03-24 18:53:25 -0600

    Added UI elements to PlayerFormVC

--- ScavengerHunt/ScavengerHunt/Controllers/PlayerFormViewController.swift
+++ ScavengerHunt/ScavengerHunt/Controllers/PlayerFormViewController.swift
@@ -9,10 +9,15 @@ import UIKit
 
 class PlayerFormViewController: UIViewController {
 
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var infoLabel: UILabel!
+    @IBOutlet weak var beginBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
 }
