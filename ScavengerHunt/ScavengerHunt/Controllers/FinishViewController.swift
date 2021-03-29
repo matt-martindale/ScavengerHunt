464edbb218b1ef2a90f39e30030f6b0a7dd57399
Matt Martindale <martindalematt18@yahoo.com>
2021-03-29 10:29:32 -0600

    Update FinishViewController.swift

--- ScavengerHunt/ScavengerHunt/Controllers/FinishViewController.swift
+++ ScavengerHunt/ScavengerHunt/Controllers/FinishViewController.swift
@@ -9,16 +9,21 @@ import UIKit
 
 class FinishViewController: UIViewController {
     
+    // MARK: - IBOutlets
     @IBOutlet weak var congratsImageView: UIImageView!
     @IBOutlet weak var homeBtn: UIButton!
     
+    // MARK: - Lifecycles
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
+    // MARK: - IBActions
     @IBAction func homeBtnTapped(_ sender: UIButton) {
     }
+    
+    // MARK: - Methods
     
 }
