c86202f349ed21636a2ddcc8ec2e060ab5afeb37
Matt Martindale <martindalematt18@yahoo.com>
2021-04-04 15:53:40 -0600

    Created fetchUserInfo method to ProfileVC

--- ScavengerHunt/ScavengerHunt/Controllers/CreatorControllers/ProfileViewController.swift
+++ ScavengerHunt/ScavengerHunt/Controllers/CreatorControllers/ProfileViewController.swift
@@ -9,11 +9,51 @@ import UIKit
 import Firebase
 
 class ProfileViewController: UIViewController {
-
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    // MARK: - Properties
+    var db = Firestore.firestore()
+    
+    // MARK: - Lifecycles
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupViews()
+    }
+    
+    // MARK: - IBActions
+    
+    // MARK: - Methods
+    func setupViews() {
+        fetchUserInfo()
+    }
+    
+    func fetchUserInfo() {
+        guard let userID = Auth.auth().currentUser?.uid else { return }
+        let userRef = db.collection("users").document(userID)
+        
+        DispatchQueue.global(qos: .default).async {
+            userRef.getDocument { [weak self] document, error in
+                guard let self = self else { return }
+                guard error == nil else {
+                    print("Error retrieving User Info: \(error!)")
+                    return
+                }
+                
+                if let document = document, document.exists {
+                    guard let data = document.data() else { return }
+                    // User Data
+                    guard let firstName = data["firstName"] as? String,
+                          let lastName = data["lastName"] as? String else { return }
+                    
+                    DispatchQueue.main.async {
+                        self.nameLabel.text = String(firstName + " " + lastName)
+                    }
+                    
+                }
+            }
+        }
     }
     
     func signOut() {
