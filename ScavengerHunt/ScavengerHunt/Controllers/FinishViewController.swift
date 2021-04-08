8a25077d519081f91802bd0d4267b6b4bd1b9a31
Matt Martindale <martindalematt18@yahoo.com>
2021-04-07 20:55:55 -0600

    Added CAEmitterLayer for celebration

--- ScavengerHunt/ScavengerHunt/Controllers/FinishViewController.swift
+++ ScavengerHunt/ScavengerHunt/Controllers/FinishViewController.swift
@@ -17,6 +17,7 @@ class FinishViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupViews()
+        createParticles()
     }
     
     override func viewWillLayoutSubviews() {
@@ -33,5 +34,43 @@ class FinishViewController: UIViewController {
         homeBtn.layer.cornerRadius = 20
         homeBtn.layer.borderWidth = 2.0
         homeBtn.layer.borderColor = UIColor.orange.cgColor
+    }
+    
+    func createParticles() {
+        let particleEmitter = CAEmitterLayer()
+
+        particleEmitter.emitterPosition = CGPoint(x: view.center.x, y: -20)
+        particleEmitter.emitterShape = .line
+        particleEmitter.emitterSize = CGSize(width: view.frame.size.width, height: 1)
+
+        let blue = makeEmitterCell(color: #colorLiteral(red: 0.3694034219, green: 0.761762321, blue: 0.896699369, alpha: 1))
+        let orange = makeEmitterCell(color: #colorLiteral(red: 1, green: 0.5419902205, blue: 0.2283754647, alpha: 1))
+        let purple = makeEmitterCell(color: #colorLiteral(red: 0.5471776128, green: 0.3385567665, blue: 0.7818789482, alpha: 1))
+        let yellow = makeEmitterCell(color: #colorLiteral(red: 0.9198206067, green: 0.8792296648, blue: 0.3015590906, alpha: 1))
+        let red = makeEmitterCell(color: #colorLiteral(red: 0.8214911819, green: 0.2103256583, blue: 0.264827013, alpha: 1))
+
+        particleEmitter.emitterCells = [blue, orange, purple, yellow, red]
+
+        view.layer.addSublayer(particleEmitter)
+    }
+
+    func makeEmitterCell(color: UIColor) -> CAEmitterCell {
+        let cell = CAEmitterCell()
+        cell.birthRate = 3
+        cell.lifetime = 7.0
+        cell.lifetimeRange = 0
+        cell.color = color.cgColor
+        cell.velocity = 200
+        cell.velocityRange = 50
+        cell.emissionLongitude = CGFloat.pi
+        cell.emissionRange = CGFloat.pi / 4
+        cell.spin = 2
+        cell.spinRange = 3
+        cell.scaleRange = 0.5
+        cell.scaleSpeed = -0.05
+        
+
+        cell.contents = UIImage(named: "confetti")?.cgImage
+        return cell
     }
 }
---
+++ ScavengerHunt/ScavengerHunt/Helpers/Colors.swift
@@ -0,0 +1,12 @@
+//
+//  Colors.swift
+//  ScavengerHunt
+//
+//  Created by Matthew Martindale on 4/7/21.
+//
+
+import UIKit
+
+struct Colors {
+    static let lightPurple = #colorLiteral(red: 0.890158236, green: 0.4124694467, blue: 0.8113290668, alpha: 1)
+}
