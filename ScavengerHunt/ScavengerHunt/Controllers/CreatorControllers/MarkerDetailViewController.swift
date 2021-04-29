17c5af960263e9646b2e09f1cfe85702ddf8a61f
Matt Martindale <martindalematt18@yahoo.com>
2021-04-28 19:49:41 -0600

    Formatted UI Elements

--- ScavengerHunt/ScavengerHunt/Controllers/CreatorControllers/MarkerDetailViewController.swift
+++ ScavengerHunt/ScavengerHunt/Controllers/CreatorControllers/MarkerDetailViewController.swift
@@ -10,21 +10,44 @@ import UIKit
 class MarkerDetailViewController: UIViewController {
     
     // MARK: - IBOutlets
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var clueLabel: UILabel!
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var clueTextView: UITextView!
+    @IBOutlet weak var saveBtn: UIButton!
+    @IBOutlet weak var errorLabel: UILabel!
     
     // MARK: - Properties
     var marker: Marker?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupTextFields()
         setupViews()
     }
     
-    private func setupViews() {
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        errorLabel.alpha = 0.0
+    }
+    
+    private func setupTextFields() {
         guard let marker = marker else { return }
-        titleLabel.text = marker.title
-        clueLabel.text = marker.clue
+        titleTextField.text = marker.title
+        clueTextView.text = marker.clue
+    }
+    
+    private func setupViews() {
+        titleTextField.layer.cornerRadius = 20
+        titleTextField.addBottomBorder()
+        clueTextView.layer.borderWidth = 2.0
+        clueTextView.layer.borderColor = UIColor.orange.cgColor
+        clueTextView.backgroundColor = .clear
+        clueTextView.layer.cornerRadius = 10
+        clueTextView.tintColor = .orange
+        saveBtn.layer.cornerRadius = 20
+        errorLabel.alpha = 0.0
     }
 
+    @IBAction func saveBtnTapped(_ sender: UIButton) {
+        Utilites.shared.playSound(sender.tag)
+    }
 }
