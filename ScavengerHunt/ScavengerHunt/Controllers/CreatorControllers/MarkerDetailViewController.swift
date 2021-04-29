6ff3298729c0ba9a2ed40977d5dfbe19fcc85133
Matt Martindale <martindalematt18@yahoo.com>
2021-04-28 18:46:06 -0600

    Added MarkerDetailVC to edit Markers

---
+++ ScavengerHunt/ScavengerHunt/Controllers/CreatorControllers/MarkerDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MarkerDetailViewController.swift
+//  ScavengerHunt
+//
+//  Created by Matthew Martindale on 4/28/21.
+//
+
+import UIKit
+
+class MarkerDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
