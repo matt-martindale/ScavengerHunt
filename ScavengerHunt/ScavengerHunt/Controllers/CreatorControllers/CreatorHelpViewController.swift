512db3dd5d4d7834bb12fcba6e89ee7b2ebda454
Matt Martindale <martindalematt18@yahoo.com>
2021-04-29 18:57:41 -0600

    Added CreatorHelpVC file

---
+++ ScavengerHunt/ScavengerHunt/Controllers/CreatorControllers/CreatorHelpViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CreatorHelpViewController.swift
+//  ScavengerHunt
+//
+//  Created by Matthew Martindale on 4/29/21.
+//
+
+import UIKit
+
+class CreatorHelpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
