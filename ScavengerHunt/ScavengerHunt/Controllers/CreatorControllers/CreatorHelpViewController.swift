bbb3c08a4c6ca0a029c81ca92a6400b0fd1d61b1
Matt Martindale <martindalematt18@yahoo.com>
2021-05-01 19:07:28 -0600

    Added NFC Tag Specs text to CreatorHelpVC

--- ScavengerHunt/ScavengerHunt/Controllers/CreatorControllers/CreatorHelpViewController.swift
+++ ScavengerHunt/ScavengerHunt/Controllers/CreatorControllers/CreatorHelpViewController.swift
@@ -10,10 +10,24 @@ import UIKit
 class CreatorHelpViewController: UIViewController {
 
     @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var nfcTagSpecsLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupViews()
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        nfcTagSpecsLabel.text = """
+        Works with any rewritable NDEF NFC tags type 1-5, or any that are compatible with “Amiibos”.
+
+        Minimum 40 bytes of memory.
+
+        Tags with adhesive are best when placing around!
+
+        Avoid placing tags on metal surfaces, metal interferes with the scanning capabilities.
+        """
     }
     
     private func setupViews() {
