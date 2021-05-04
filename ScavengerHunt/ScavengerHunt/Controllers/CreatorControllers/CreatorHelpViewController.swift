9616c637301b32d4acb4984a66ae55896c0927c6
Matt Martindale <martindalematt18@yahoo.com>
2021-05-03 21:41:23 -0600

    Adding programmatic scrollView

--- ScavengerHunt/ScavengerHunt/Controllers/CreatorControllers/CreatorHelpViewController.swift
+++ ScavengerHunt/ScavengerHunt/Controllers/CreatorControllers/CreatorHelpViewController.swift
@@ -9,44 +9,51 @@ import UIKit
 
 class CreatorHelpViewController: UIViewController {
 
-    @IBOutlet weak var scrollView: UIScrollView!
-    @IBOutlet weak var nfcTagSpecsLabel: UILabel!
+    // MARK: - IBOutlets
     
+    // MARK: - Properties
+    var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        return scrollView
+    }()
+    
+    // MARK: - Lifecycles
     override func viewDidLoad() {
         super.viewDidLoad()
         setupViews()
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-    }
-    
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
-        scrollView.contentSize = CGSize(width: 400, height: 2300)
-        nfcTagSpecsLabel.text = """
-        Works with any rewritable NDEF NFC tags type 1-5, or any that are compatible with “Amiibos”.
-
-        Minimum 40 bytes of memory.
-
-        Tags with adhesive are best when placing around!
-
-        Avoid placing tags on metal surfaces, metal interferes with the scanning capabilities.
-
-
-        Works with any rewritable NDEF NFC tags type 1-5, or any that are compatible with “Amiibos”.
-
-        Minimum 40 bytes of memory.
-
-        Tags with adhesive are best when placing around!
-
-        Avoid placing tags on metal surfaces, metal interferes with the scanning capabilities.
-        """
+        scrollView.frame = CGRect(x: 20, y: 150, width: view.frame.size.width-40, height: 2000)
+        scrollView.contentSize = CGSize(width: view.frame.size.width-40, height: 2000)
+        scrollView.backgroundColor = .gray
+        scrollView.layer.cornerRadius = 10
+        view.addSubview(scrollView)
+        
+//        """
+//        Works with any rewritable NDEF NFC tags type 1-5, or any that are compatible with “Amiibos”.
+//
+//        Minimum 40 bytes of memory.
+//
+//        Tags with adhesive are best when placing around!
+//
+//        Avoid placing tags on metal surfaces, metal interferes with the scanning capabilities.
+//
+//
+//        Works with any rewritable NDEF NFC tags type 1-5, or any that are compatible with “Amiibos”.
+//
+//        Minimum 40 bytes of memory.
+//
+//        Tags with adhesive are best when placing around!
+//
+//        Avoid placing tags on metal surfaces, metal interferes with the scanning capabilities.
+//        """
     }
     
+    // MARK: - Methods
     private func setupViews() {
-        scrollView.layer.cornerRadius = 10
     }
 
 }
