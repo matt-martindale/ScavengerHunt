b437e5976bd9d32c364878795508b76ca852dbfe
Matt Martindale <martindalematt18@yahoo.com>
2021-05-09 09:58:21 -0600

    Changed TabBarTintColor

--- ScavengerHunt/ScavengerHunt/Resources/AppDelegate.swift
+++ ScavengerHunt/ScavengerHunt/Resources/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
         UserDefaults.standard.setValue(false, forKey: Constants.inCreatorModeKey)
+        UITabBar.appearance().tintColor = .orange
         return true
     }
 
